aa9bfbe4c17e58a2a74b31c7787684c513dd4a36
dianagamez2809 <diana.gamez.fon@gmail.com>
2013-07-16 15:49:34 +0000

    nuevo grader

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -40,12 +43,17 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioUrlFile = function(htmlfile) {
+    return cheerio.load(htmlfile);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+
+
+var checkCheerioFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,18 +69,36 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function() {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            var checkJson = checkCheerioFile(cheerioUrlFile(result), program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
+    };
+    return response2console;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_adress>', 'The URL to show the application', undefined)
         .parse(process.argv);
 
-	if (program.url) {
-		var checkJson = checkUrl(program.url, program.checks);
-	}
-	else var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    // Check if a URL has been given as an argument
+    if(program.url === undefined)  {
+      var checkJson = checkCheerioFile(cheerioHtmlFile(program.file), program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+     }
+    else {
+      var response2console = buildfn();
+      rest.get(program.url).on('complete', response2console);  
+    }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkCheerioFile = checkCheerioFile;
 }
